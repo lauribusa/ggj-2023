ed29da61bad2833ed7e176f5d5388bd51dcdf5da
Breniak <kyllian_bocklandt@hotmail.com>
2023-01-28 18:37:57 +0100

    add main cell prefab

---
+++ Scenes/Prefabs/Cell.cs
@@ -0,0 +1,46 @@
+using Godot;
+
+namespace Game;
+
+[Tool]
+public partial class Cell : Node3D
+{
+    #region Exposed
+
+    [Export]
+    private Node3D _neutralCell;
+
+    [Export]
+    private Node3D _defenseCell;
+
+    [Export]
+    private Node3D _corruptCell;
+
+    #endregion
+
+
+    #region Main
+
+    public void SetNeutral()
+    {
+        _neutralCell.Visible = true;
+        _defenseCell.Visible = false;
+        _corruptCell.Visible = false;
+    }
+
+    public void SetCorrupt()
+    {
+        _neutralCell.Visible = false;
+        _defenseCell.Visible = false;
+        _corruptCell.Visible = true;
+    }
+
+    public void SetImmune()
+    {
+        _neutralCell.Visible = false;
+        _defenseCell.Visible = true;
+        _corruptCell.Visible = false;
+    }
+
+    #endregion
+}
