79ea662f95d89e291ea179d5a0f3481cf7569768
LAPTOP-5QR69DH2\Lauri <lauri.busa@gmail.com>
2023-01-28 22:04:50 +0100

    added UI

---
+++ Assets/Scripts/GameNodeUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class GameNodeUI : MonoBehaviour
+{
+	#region Exposed
+	
+	#endregion
+
+	
+	#region Private And Protected
+   	
+	#endregion
+
+	
+	#region Unity API
+	
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    
+    #endregion
+    
+
+    #region Main
+    
+    #endregion
+
+
+    #region Singleton
+
+    #endregion
+}
+
